74c88afce7811712cb38134daea640c6bbd3e1bf
oziel <oziel.silva222@gmail.com>
2020-12-23 01:39:19 -0300

    feito o desafio

--- room-homepage-master/js/script.js
+++ room-homepage-master/js/script.js
@@ -8,23 +8,60 @@ $('.fade').slick({
   nextArrow: $("#arrow-next")
 });
 
-$(document).ready(function(){
-  let i = 0;
-  for(i = 0; i < 3; i++) {
-    if( i === 1) {
-      $("#arrow-next").click(function(){
-        $(".bg-card-01").css("background-image", "url(img/desktop-image-hero-2.jpg)");
-      });
-    }
-    else if( i === 2) {
-      $("#arrow-next").click(function(){
-        $(".bg-card-01").css("background-image", "url(img/desktop-image-hero-2.jpg)");
-      });
-    }
-    else if( i === 3) {
-      $("#arrow-next").click(function(){
-        $(".bg-card-01").css("background-image", "url(img/desktop-image-hero-3.jpg)");
-      });
-    }
+const bgCard01 = document.querySelector('.bgCard01')
+const arrowLeft = document.querySelector('#arrow-prev');
+const arrowRight = document.querySelector('#arrow-next');
+
+const data = [
+  {
+    desktopImage: 'img/desktop-image-hero-1.jpg'
+  },
+  {
+    desktopImage: 'img/desktop-image-hero-2.jpg'
+  },
+  {
+    desktopImage: 'img/desktop-image-hero-3.jpg'
   }
+];
+
+
+let currentPage = 0;
+
+arrowLeft.addEventListener('click', () => {
+  currentPage--
+  if(currentPage < 0){
+      currentPage = data.length - 1;
+  }
+  bgCard01.style.backgroundImage = `url(${data[currentPage].desktopImage})`;
+  effect.style.backgroundImage = effectTransition[currentPage].transition;
+  console.log(currentPage);
 });
+
+arrowRight.addEventListener('click', () => {
+  currentPage++
+  if(currentPage > data.length-1){
+      currentPage = 0;
+  }  
+  bgCard01.style.backgroundImage = `url(${data[currentPage].desktopImage})`; 
+  console.log(currentPage); 
+});
+
+window.onload = () => {
+  currentPage = 0;
+  bgCard01.style.backgroundImage = `url(${data[currentPage].desktopImage})`; 
+  console.log(currentPage);
+}
+
+//mobile scripts
+const navbar = document.querySelector(".navbar");
+const ham = document.querySelector(".ham");
+
+ham.addEventListener("click", toggleHamburger);
+
+function toggleHamburger() {
+  navbar.classList.toggle("showNav");
+  ham.classList.toggle("showClose");
+}
+
+
+

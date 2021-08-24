3e2efa740f44dc8374baecf0a81339fecae030e0
oziel <oziel.silva222@gmail.com>
2021-08-24 00:06:14 -0300

    feito animação fadeInRight e fadeInUp

--- sunnyside-agency-landing-page-main/js/script.js
+++ sunnyside-agency-landing-page-main/js/script.js
@@ -1,14 +1,45 @@
-const cardDescription = document.querySelectorAll('.js-scroll');
-const widowMiddle = window.innerHeight * 0.7;
 
-function animaScroll() {
-  cardDescription.forEach((description) => {
-    const descriptionTop = description.getBoundingClientRect().top;
-    const isCardVisible = (descriptionTop - widowMiddle) < 0;
-    if(isCardVisible) {
-      description.classList.add('ativo');
+function initAnimacaoScroll() {
+  const cardDescription01 = document.querySelectorAll('[data-anima="scrollFadeInRight"]');
+  const cardDescription02 = document.querySelectorAll('[data-anima="scrollFadeInUp"]');
+
+  if (cardDescription01.length) {
+    const widowMiddle = window.innerHeight * 0.9;
+
+    function animaScrollfadeInRight() {
+      cardDescription01.forEach((description) => {
+        const descriptionTop = description.getBoundingClientRect().top;
+        const isCardVisible = (descriptionTop - widowMiddle) < 0;
+        if(isCardVisible) {
+          description.classList.add('animate__fadeInRight');
+        }
+        
+      });
     }
-  });
-}
+  }
 
-window.addEventListener('scroll', animaScroll);
+  if (cardDescription02.length) {
+    const widowMiddle = window.innerHeight * 0.8;
+    function animaScrollfadeInUp() {
+      cardDescription02.forEach((description) => {
+        const descriptionTop = description.getBoundingClientRect().top;
+        const isCardVisible = (descriptionTop - widowMiddle) < 0;
+        if(isCardVisible) {
+          description.classList.add('animate__fadeInUp');
+        }
+        
+      });
+    }
+  }
+    
+
+    
+    window.addEventListener('scroll', animaScrollfadeInRight);
+    window.addEventListener('scroll', animaScrollfadeInUp);
+    
+
+}
+initAnimacaoScroll();
+
+
+
